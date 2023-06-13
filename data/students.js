3d5fea3491822da2f66a32934958f67ae018acbf
jcoop32 <jcooper11@hawk.iit.edu>
2023-06-13 14:44:45 -0500

    init

---
+++ data/students.js
@@ -0,0 +1,16 @@
+const students = [
+  'LeBron James',
+  'Stephen Curry',
+  'Micheal Jordan',
+  'DeMar DeRozan',
+  'Zach LaVine',
+  'Kevin Durant',
+  'Mikal Bridges',
+  'Lonzo Ball',
+];
+
+module.exports = {
+  getAll: function () {
+    return students;
+  },
+};
